693a7969678c4060613482655dd6385fe6fddcbc
mdz <tailor@hades.madism.org>
2002-01-14 02:44:03 +0100

    [project @ 2002-01-14 02:42:21 by mdz] Argh...maxsplit is the number of splits, not the number of fields

--- apt_listchanges.py
+++ apt_listchanges.py
@@ -193,7 +193,7 @@ def read_apt_pipeline():
         if not aptconfig or aptconfig == '\n':
             break
 
-        (option, value, rest) = aptconfig.rstrip().split('=', 2)
+        (option, value) = aptconfig.rstrip().split('=', 1)
         
         if option == 'quiet':
             config.quiet = value
