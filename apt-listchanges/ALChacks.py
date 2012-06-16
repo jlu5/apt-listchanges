ed435c44113706ce0b5d1f5441c1699278c02e20
Sandro Tosi <morph@debian.org>
2012-06-16 23:21:52 +0200

    Don't use _() if we can't set requested locale; thanks to Ansgar Burchardt for the report and to Stuart Prescott for the patch; Closes: #602176

--- apt-listchanges/ALChacks.py
+++ apt-listchanges/ALChacks.py
@@ -29,7 +29,7 @@ import sys
 try:
     locale.setlocale(locale.LC_ALL, '')
 except locale.Error:
-    sys.stderr.write(_("Can't set locale; make sure $LC_* and $LANG are correct!\n"))
+    sys.stderr.write("Can't set locale; make sure $LC_* and $LANG are correct!\n")
     locale.setlocale(locale.LC_ALL, 'C')
 
 def _(x):
